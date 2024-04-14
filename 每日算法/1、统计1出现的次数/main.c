02e1ba7aadee60114fe9abd05bad8391576de2db
abc12137 <144017707+abc12137@users.noreply.github.com>
2024-04-14 20:15:24 +0800

    Add files via upload

--- 每日算法/1、统计1出现的次数/main.c
+++ 每日算法/1、统计1出现的次数/main.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+int main() {
+        int n; 
+        int count=0;
+        int cur,high,low;
+        printf("please input n \n");
+        scanf("%d",&n);
+        long long i=1;
+        while(i<=n){
+         cur=(n/i)%10;
+         high=n/(10*i);
+         low=n%i;
+         if(cur==0){
+              count+=high*i;
+           }  else if(cur==1){
+                 count+=high*i+low+1;
+              }  else{                    //9>cur>=2
+                     count+=(high+1)*i;
+                   }
+          i=i*10;
+        }
+    printf("%d",count);
+}
